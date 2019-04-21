8e4371b05c47819191d8d2707a0a1098e4c5b98a
Kaige Ye <ye@kaige.org>
2019-04-21 11:39:03 +0800

    it works

---
+++ mq.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+const http = require('http');
+const fs = require('fs');
+
+const mqs = new WebSocket.Server({ port: 2081 });
+
+mqs.on('connection', function incoming(client) {
+    client.on('message', function(message) {
+        console.log(new Date().toISOString() + ' broadcast: ' + message + " clients.size " + mqs.clients.size);
+        mqs.clients.forEach(ws => {
+            if (ws.readyState === 1 && ws !== client) {
+                ws.send(message);
+                console.log(new Date().toISOString() + ' broadcast: ' + message);
+            }
+        })
+    })
+});
--- push.js
+++ push.js
@@ -3,64 +3,94 @@ const http = require('http');
 const fs = require('fs');
 
 const wss = new WebSocket.Server({ port: 2080 });
-const mq = new WebSocket('127.0.0.1:2081');
+const mq = new WebSocket('ws://127.0.0.1:2081');
 
 function auth(msg) {
     return true;
 }
 
-function getUSerInfo(msg) {
-    return msg.user;
+const Util = {
+    getUserInfo: function (msg) {
+        msg = JSON.parse(msg);
+        return msg.user;
+    }
+}
+
+const DB = {
+    get: function(topic, callback) {
+        const users = [];
+        callback(users);
+    }
 }
 
 const Redis = {
+    kv: {},
     get: function(key) {
         // Redis.get
+        console.log('::Redis.get ');
+        return Redis.kv[key];
     },
     put: function(k, v) {
         // Redis.put
+        console.log('::Redis.put ');
+        Redis.kv[k] = v;
+    },
+    remove: function(k) {
+        delete Redis.kv[k];
     }
 };
 
 const Registry = {
     register: function(userInfo, ws) {
+        console.log(new Date().toISOString() + ' Register: ' + JSON.stringify(userInfo));
+        
         let list = Redis.get(userInfo.userId);
+        list = list ? list : new Set();
         list.add(ws);
         Redis.put(userInfo.userId, list);
 
         Redis.put(ws, userInfo.userId);
+        ws.send('registered');
     },
     delete: function(ws) {
         const userId = Redis.remove(ws);
         let list = Redis.get(userId);
-        list.remove(ws);
-        Redis.put(userInfo.userId, list);
+        if (list) {
+            list.delete(ws);
+            Redis.put(userInfo.userId, list);
+        }
     },
     findConnByUserId: function(userId) {
+        console.log('::findConnByUserId ', userId);
         return Redis.get(userId);
     },
     findConnByTopic: function(topic) {
         let list = Redis.get(topic);
         if (list === null) {
-            list = [];
-            const users = DB.get(topic);
-            users.forEach(user => {
-                list.addAll(findConnByUserId());
-            })
-            Redis.put(topic, list);
+            DB.get(topic, function(users) {
+                list = [];
+                users.forEach(user => {
+                    list.addAll(findConnByUserId());
+                })
+                Redis.put(topic, list);
+            });
+            
         }
         return list;
     },
     getRouter: function(msg) {
-        return routerFunc[msg.type](msg);
-    },
-    routerFunc: {
-        user: function(msg) {
-            return findConnByUserId(msg.userId);
-        },
-        topic: function(msg) {
-            return findConnByTopic(msg.topic);
-        }
+        const routerFunc = {
+            user: function(msg) {
+                return Registry.findConnByUserId(msg.userId);
+            },
+            topic: function(msg) {
+                return Registry.findConnByTopic(msg.topic);
+            }
+        };
+        msg = JSON.parse(msg);
+        const list = routerFunc[msg.type](msg);
+        console.log('::getRouter ' + msg.type, JSON.stringify(msg), list.size);
+        return list ? list : new Set();
     }
 }
 // ----------------------------------------------------------------
@@ -78,7 +108,7 @@ mq.on('message', function incoming(message) {
 
 // ----------------------------------------------------------------
 
-wss.on('connection', function connection(ws) {
+function unused() {
     let t;
     function notify() {
         if (ws.readyState === 1) {
@@ -88,10 +118,14 @@ wss.on('connection', function connection(ws) {
         }
     }
     t = setInterval(notify, 300);
+}
+
+wss.on('connection', function connection(ws) {
+    console.log(new Date().toISOString() + ' new conn: ');
 
     ws.on('message', function incoming(message) {
-        if (auth(msg)) {
-            const userInfo = getUserInfo(msg);
+        if (auth(message)) {
+            const userInfo = Util.getUserInfo(message);
             Registry.register(userInfo, ws);
         }
         //console.log('received: %s', message);
@@ -121,4 +155,3 @@ http.createServer(function (request, response) {
 
 // 终端打印如下信息
 console.log('Server running at http://127.0.0.1:20078/');
-
