830966356ff164396393f3384b3f1f771006e72d
Kaige Ye <ye@kaige.org>
2019-04-25 21:45:55 +0800

    clean up

--- client.js
+++
@@ -1,4 +0,0 @@
-const WebSocket = require('ws');
-
-const mq = new WebSocket('ws://ahk.altx.top:29081', {localAddress: '192.168.199.167', localPort: 55555});
-const mq2 = new WebSocket('ws://ahk.altx.top:29081', {localAddress: '192.168.199.168', localPort: 55555});
--- client2.js
+++
@@ -1,3 +0,0 @@
-const WebSocket = require('ws');
-
-const mq = new WebSocket('ws://ahk.altx.top:29081', {localAddress: '192.168.199.168'});
